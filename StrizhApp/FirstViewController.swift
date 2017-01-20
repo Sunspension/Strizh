8ad24704a9c5e0945e5712faad3feb45077252b0
Vladimir Kokhanevich <vladimir.kokhanevich@gmail.com>
2017-01-21 00:50:31 +0300

    Initial Commit

---
+++ StrizhApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  StrizhApp
+//
+//  Created by Vladimir Kokhanevich on 21/01/2017.
+//  Copyright © 2017 Vladimir Kokhanevich. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ StrizhApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  StrizhApp
+//
+//  Created by Vladimir Kokhanevich on 21/01/2017.
+//  Copyright © 2017 Vladimir Kokhanevich. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
